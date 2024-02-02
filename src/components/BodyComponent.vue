5f223412e288a9ef09f87ad80b867834c04cf1b5
LeeeYumin <flet39@daum.net>
2024-02-02 18:06:43 +0900

    board

---
+++ src/components/BodyComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul id="myUL">
+    <li>Hit the gym <span class="close">X</span></li>
+    <li class="checked">Pay bills<span class="close">X</span></li>
+    <li>Meet George <span class="close">X</span></li>
+    <li>Buy eggs <span class="close">X</span></li>
+    <li>Read a book <span class="close">X</span></li>
+    <li>Organize office <span class="close">X</span></li>
+  </ul>
+</template>
+
+<script>
+export default {
+
+}
+</script>
